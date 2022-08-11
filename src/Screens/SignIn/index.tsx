690b69ff907138ef877d67935b58cc44d0c63895
Jonathan Erthal <jonierthal@gmail.com>
2022-08-11 12:53:11 -0300

    add Login c/Google

--- src/Screens/SignIn/index.tsx
+++ src/Screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import AppleSvg from '../../assets/apple.svg';
@@ -20,8 +21,17 @@ import {
 } from './styles';
 
 export function SignIn(){
-    const { user } = useAuth();
-    console.log(user.name);
+    const { signInWithGoogle } = useAuth();
+
+    async function handeSignInWithGoogle(){
+        try {
+            await signInWithGoogle();
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Não foi possível conectar com google')
+        }
+    }
+   
     return(
         <Container>
             <Header>
@@ -49,6 +59,7 @@ export function SignIn(){
                     <SignInSocialButton
                         title="Entrar com Google"
                         svg={GoogleSvg}
+                        onPress={handeSignInWithGoogle}
                     />
 
                     <SignInSocialButton
